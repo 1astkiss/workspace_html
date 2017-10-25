d863b96544593351e48dd63efdeac655b2767aaa
bettertoday <bettertoday@gmail.com>
2017-10-26 00:17:16 +0900

    ..

---
+++ ModernWeb-JS/ch21/server.js
@@ -0,0 +1,11 @@
+﻿var express = require('express');
+
+var app = express();
+
+app.use(function (request, response) {
+    response.send('<meta charset="utf-8" /><h1>Hello 안녕!!</h1>');
+});
+
+app.listen(52273, function () {
+    console.log('Server Running at http://127.0.0.1:52273');
+});
