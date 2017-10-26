a6eb6b66207693bdbe80a254375f5902c4fbeecf
1astkiss <bettertoday@gmail.com>
2017-10-26 18:42:37 +0900

    ..

--- ModernWeb-JS/ch21/server.js
+++ ModernWeb-JS/ch21/server.js
@@ -1,10 +1,87 @@
 ﻿var express = require('express');
+var bodyParser = require('body-parser');
+
+var items = [{
+    name: '우유',
+    price: '2000'
+}, {
+    name: '홍차',
+    price: '5000'
+}, {
+    name: '커피',
+    price: '5000'
+}];
 
 var app = express();
 
-app.use(function (request, response) {
-    response.send('<meta charset="utf-8" /><h1>Hello 안녕!!</h1>');
+app.use(express.static('public'));
+
+app.all('/data.html', function (request, response) {
+    var output = '';
+    output += '<DOCTYPE html>';
+    output += '<html>';
+    output += '<head>';
+    output += '     <title>Data HTML</title>';
+    output += '</head>';
+    output += '<body>';
+
+    items.forEach(function (item) {
+        output += '<div>';
+        output += '     <h1>' + item.name + '</h1>';
+        output += '     <h2>' + item.price + '</h2>';
+        output += '</div>';
+    });
+
+    output += '</body>';
+    output += '</html>';
+
+    response.send(output);
 });
+app.all('/data.json', function (request, response) {
+    response.send(items);
+});
+app.all('/data.xml', function (request, response) {
+    var output = '';
+    output += '<?xml version="1.0" encoding="UTF-8" ?>';
+    output += '<products>';
+    items.forEach(function (item) {
+        output += '<product>';
+        output += '     <name>' + item.name + '</name>';
+        output += '     <price>' + item.price + '</price>';
+        output += '<product>';
+    });
+
+    output += '</products>';
+    response.type('text/xml');
+    response.send(output);
+});
+
+app.all('/parameter/:name/:region', function (request, response) {
+    var name = request.params.name;
+    var region = request.params.region;
+    response.send('<h1>' + name + ':' + region + '</h1>');
+});
+
+
+//app.use(function (request, response) {
+//    response.send('<h1>Hello Middleware...!</h1>');
+//});
+
+//app.use(function (request, response, next) {
+//    request.test = 'request.test from middleware';
+//    response.test = 'response.text from middleware';
+//    console.log('first');
+//    next();
+//});
+
+//app.use(function (request, response, next) {
+//    console.log('second');
+//    next();
+//});
+
+//app.use(function (request, response, next) {
+//    response.send('<h1>' + request.test + '</h1>\n<h2>' + response.test + '</h2>');
+//});
 
 app.listen(52273, function () {
     console.log('Server Running at http://127.0.0.1:52273');
---
+++ ModernWeb-JS/ch21/server21.14.js
@@ -0,0 +1,92 @@
+﻿var express = require('express');
+var bodyParser = require('body-parser');
+
+var items = [{
+    name: '우유',
+    price: '2000'
+}, {
+    name: '홍차',
+    price: '5000'
+}, {
+    name: '커피',
+    price: '5000'
+}];
+
+var app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/products', function (request, response) {
+    response.send(items);
+});
+app.get('/products/:id', function (request, response) {
+    var id = Number(request.params.id);
+
+    if (isNaN(id)) {
+        response.send({
+            error: '숫자를 입력하세요!'
+        });
+    } else if (items[id]) {
+        response.send(items[id]);
+    } else {
+        response.send({
+            error: '존재하지 않는 데이터입니다!'
+        });
+    }
+});
+app.post('/products', function (request, response) {
+    var name = request.body.name;
+    var price = request.body.price;
+    var item = {
+        name: name,
+        price: price
+    }
+
+    items.push(item);
+    response.send({
+        message: '데이터를 추가했습니다.',
+        data: item
+    });
+});
+app.put('/products/:id', function (request, response) {
+    var id = Number(request.params.id);
+    var name = request.body.name;
+    var price = request.body.price;
+
+    if (items[id]) {
+        if (name) { items[id].name = name; }
+        if (price) { items[id].price = price; }
+
+        response.send({
+            message: '데이터를 수정했습니다.',
+            data: items[id]
+        });
+    } else {
+        response.send({
+            error: '존재하지 않는 데이터입니다!'
+        });
+    }
+});
+
+app.delete('/products/:id', function (request, response) {
+    var id = Number(request.params.id);
+
+    if (isNaN(id)) {
+        response.send({
+            error: '숫자를 입력하세요'
+        });
+    } else if (items[id]) {
+        items.splice(id, 1);
+        response.send({
+            message: '데이터를 삭제했습니다!'
+        });
+    } else {
+        response.send({
+            error: '존재하지 않는 데이터입니다!'
+        });
+    }
+});
+
+app.listen(52273, function () {
+    console.log('Server Running at http://127.0.0.1:52273');
+});
